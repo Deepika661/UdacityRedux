7a0723bc2fb3bd4a78ce4cdee8a4f916d99a49d4
Deepika <deepika@github.com>
2018-05-11 10:27:06 +0530

    NOTFOUND

---
+++ frontend/src/components/NotFoundError.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFoundError = () => {
+  return (
+    <div className="App">
+      <div className="text-center">
+        <h5>Page not found</h5>
+      </div>
+    </div>
+  )
+};
+
+export default NotFoundError;
